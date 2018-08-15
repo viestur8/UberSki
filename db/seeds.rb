cf99d2b88a4ecfd4979ad3961cbdd36745a9fd91
viestur8 <twotigers@vivaldi.net>
2018-08-15 11:10:58 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,16 @@ User.create(first_name: 'Arnold',
    last_name: 'Schwarzenneger',
    email: 'arny@california.gov',
    username: 'arnoldK',
-   password: 'guest'
-   birthday:07/30/1947
+   password: 'guest',
+   birthday: '07/30/1947'
  )
+
  User.create(first_name: 'Ray',
     last_name: 'Gillette',
     email: 'ray@isis.org',
     username: 'arnoldK',
-    password: 'guest'
-    birthday:07/30/1947
+    password: 'guest',
+    birthday: '07/30/1947'
   )
 
 Blogpost.create(title: 'Young Snow', post: 'txt here', timestamp: 'today', user_id:1 )
-
-t.string :title
-t.text :post
-t.timestamp :timestamp
-t.integer :user_id
